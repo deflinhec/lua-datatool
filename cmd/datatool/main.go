41caaa9cc855e7717a041b5731427c35ee0ae6a9
danny.wang <danny.wang@roycetechnology.com>
2023-06-01 01:14:51 +0200

    update license

--- cmd/datatool/main.go
+++ cmd/datatool/main.go
@@ -1,3 +1,17 @@
+// Copyright 2023 Deflinhec
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -9,16 +23,16 @@ import (
 	"path/filepath"
 	"strings"
 
-	doc "datatool.deflinhec.dev/doc"
-	"datatool.deflinhec.dev/editor"
-	gext "datatool.deflinhec.dev/internal/giuext"
 	g "github.com/AllenDang/giu"
+	doc "github.com/deflinhec/lua-datatool/doc"
+	"github.com/deflinhec/lua-datatool/editor"
+	gext "github.com/deflinhec/lua-datatool/internal/giuext"
 	"github.com/jessevdk/go-flags"
 	"github.com/ncruces/zenity"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 
-	_ "datatool.deflinhec.dev/internal/translations"
+	_ "github.com/deflinhec/lua-datatool/internal/translations"
 )
 
 var (
